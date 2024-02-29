d6fb7dc46b631cff5e99f3b9613e981935c6256c
Gergely Both <11244975-gergely.both@users.noreply.gitlab.com>
2024-02-29 14:43:41 +0100

    some simplification

--- chess_utils/board_mechanics.py
+++ chess_utils/board_mechanics.py
@@ -1,4 +1,4 @@
-# TODO: replace strings with objs (dict call obj / str+repr()), move to another git branch, try to spread things to new files, make venv + install tkinter, 
+# TODO: replace strings with objs (dict call obj / str+repr()), try to spread things to new files, make venv + install tkinter, non-string class instance references, 
 
 import chess_utils.board_params as bp
 import tkinter as tk
@@ -9,11 +9,11 @@ import os
 
 
 RESOURCE_PATH = "chess_pieces/"
-NumberedEquivalent = namedtuple("NumberedEuivalent", ["x", "y"])
+NumberedEquivalent = namedtuple("NumberedEquivalent", ["x", "y"])
 square_names_objs = {}
 figure_names_objs = {}
 figures_squares_orig = {}
-figures_squares_now = {} # =figures_squares_orig.copy()
+figures_squares_now = {}
 
 
 #########################################
@@ -22,33 +22,33 @@ figures_squares_now = {} # =figures_squares_orig.copy()
 
 
 class Square:
-    def __init__(self, name, coordinates, central_coordinates, numbered_equivalent):
+    def __init__(self, name, all_coordinates, central_coordinates):
         self.name = name
-        self.coordinates = coordinates
+        self.all_coordinates = all_coordinates
         self.central_coordinates = central_coordinates
-        self.numbered_equivalent = numbered_equivalent
+        self.numerically = NumberedEquivalent(bp.numeric_equivalent[name[0]], int(name[1]))
 
     def __repr__(self):
         return self.name
 
     @classmethod
-    def repr_lettered(cls, num_pair):
+    def alphabetically(cls, num_pair):
         """returns square name from numeric equivalent"""
         if len(num_pair) == 2:
             for obj in square_names_objs.values():
-                if obj.numbered_equivalent == num_pair:
+                if obj.numerically == num_pair:
                     return obj
 
     @classmethod
     def detect_square(cls, coordinates):
-        """detects square by clicked coordinates"""
-        for square, obj in square_names_objs.items():
-            if coordinates in obj.coordinates:
+        """returns square object of clicked coordinates"""
+        for obj in square_names_objs.values():
+            if coordinates in obj.all_coordinates:
                 return obj
 
     @classmethod
     def square_not_owned(cls, square):
-        """scans if square is (already) occupied by own piece"""
+        """checks/scans if square is already occupied by own piece"""
         for dict_figure, dict_square in figures_squares_now.items():
             if square is dict_square and dict_figure.color is cls.game_instance.chosen_figure.color:
                 return False
@@ -78,30 +78,32 @@ class Figure:
         return hash(self) == hash(other)
 
     def passage_free(self, initial, target, positions_dict):
-        """scans if any square between initial and target occupied by any side's piece"""
+        """checks/scans if any square between initial and target squares is occupied by any piece"""
         x = list(range(initial.x + 1, target.x) or range(initial.x - 1, target.x, -1))
         y = list(range(initial.y + 1, target.y) or range(initial.y - 1, target.y, -1))
         if x and y:
             xy = list(zip(x, y))
             for num_pair in xy:
-                if Square.repr_lettered(num_pair) in positions_dict.values():
+                if Square.alphabetically(num_pair) in positions_dict.values():
                     return False
         elif x or y:
             xy = list(zip(cycle([target.x]), y) if len(x) < len(y) else zip(x, cycle([target.y])))
             for num_pair in xy:
-                if Square.repr_lettered(num_pair) in positions_dict.values():
+                if Square.alphabetically(num_pair) in positions_dict.values():
                     return False
         return True
 
     @classmethod
     def detect_figure(cls, selected_square):
-        """detects piece by selected square"""
+        """returns figure object on user-selected square object"""
         for figure, square in figures_squares_now.items():
             if selected_square is square:
                 return figure
 
     def detect_enemy(self, selected_square):
-        return enemy_piece if ((enemy_piece := Figure.detect_figure(selected_square)) and enemy_piece.color != self.color) else None
+        detected_piece = Figure.detect_figure(selected_square)
+        enemy_piece = detected_piece if detected_piece.color != self.color else None
+        return enemy_piece
 
 
 class Pawn(Figure):
@@ -109,8 +111,8 @@ class Pawn(Figure):
         super().__init__(color, kind, name, number)
 
     def validate_move(self, initial_square, target_square, positions_dict):
-        """validates pawn moves: colors: initial double move / simple move / capturing: simple / en passant"""
-        initial, target = initial_square.numbered_equivalent, target_square.numbered_equivalent
+        """validates pawn moves: by both colors, initial double move OR simple move, capturing simply OR en passant"""
+        initial, target = initial_square.numerically, target_square.numerically
         if self.color == Color.WHITE:
             if initial.y == 2 and target.y == 4 and initial.x == target.x \
                     and target_square not in positions_dict.values() \
@@ -123,7 +125,7 @@ class Pawn(Figure):
                 if self.detect_enemy(target_square):
                     return True
                 elif initial.y == 5:
-                    enpass_location = Square.repr_lettered((target.x, target.y - 1))
+                    enpass_location = Square.alphabetically((target.x, target.y - 1))
                     enpass_enemy = self.detect_enemy(enpass_location)
                     if enpass_enemy and isinstance(enpass_enemy, Pawn) and {enpass_enemy, str(figures_squares_orig[enpass_enemy])}.issubset(self.game_instance.log[-1]):
                         self.game_instance.enpass_enemy = enpass_enemy
@@ -139,7 +141,7 @@ class Pawn(Figure):
                 if self.detect_enemy(target_square):
                     return True
                 elif initial.y == 4:
-                    enpass_location = Square.repr_lettered((target.x, target.y + 1))
+                    enpass_location = Square.alphabetically((target.x, target.y + 1))
                     enpass_enemy = self.detect_figure(enpass_location)
                     if enpass_enemy and isinstance(enpass_enemy, Pawn) and {enpass_enemy, str(figures_squares_orig[enpass_enemy])}.issubset(self.game_instance.log[-1]):
                         self.game_instance.enpass_enemy = enpass_enemy
@@ -151,27 +153,27 @@ class King(Figure):
         super().__init__(color, kind, name, number)
 
     def validate_move(self, initial_square, target_square, positions_dict):
-        """simple king move / user-selected castling (not-offensive so not calculated as such): short, long"""
+        """validates king moves: single move OR castling (short or long: user-selected non-offensive)"""
         # NOTE: can not castle from check, through it (both checked below), nor into it (checked back in main cycle later), also entire passage must be free
-        initial, target = initial_square.numbered_equivalent, target_square.numbered_equivalent
+        initial, target = initial_square.numerically, target_square.numerically
         if abs(target.x - initial.x) <= 1 and abs(target.y - initial.y) <= 1:
             return True
         elif self == self.game_instance.chosen_figure and abs(target.x - initial.x) == 2 and target.y - initial.y == 0:
             if self.game_instance.chosen_figure not in chain(*self.game_instance.log) and self.in_safety(self.game_instance.chosen_figure, target_square=self.game_instance.initial_square):
                 if target.x > initial.x:
-                    castle_midpoint_square = Square.repr_lettered((target.x - 1, target.y))
+                    castle_midpoint_square = Square.alphabetically((target.x - 1, target.y))
                     if self.game_instance.chosen_figure.color == Color.WHITE and "white_rook_2" not in chain(*self.game_instance.log) and "white_rook_2" in positions_dict:
-                        rook_position_num = figures_squares_now["white_rook_2"].numbered_equivalent
+                        rook_position_num = figures_squares_now["white_rook_2"].numerically
                     elif self.game_instance.chosen_figure.color == Color.BLACK and "black_rook_2" not in chain(*self.game_instance.log) and "black_rook_2" in positions_dict:
-                        rook_position_num = figures_squares_now["black_rook_2"].numbered_equivalent
+                        rook_position_num = figures_squares_now["black_rook_2"].numerically
                     else:
                         return False
                 elif target.x < initial.x:
-                    castle_midpoint_square = Square.repr_lettered((target.x + 1, target.y))
+                    castle_midpoint_square = Square.alphabetically((target.x + 1, target.y))
                     if self.game_instance.chosen_figure.color == Color.WHITE and "white_rook_1" not in chain(*self.game_instance.log) and "white_rook_1" in positions_dict:
-                        rook_position_num = figures_squares_now["white_rook_1"].numbered_equivalent
+                        rook_position_num = figures_squares_now["white_rook_1"].numerically
                     elif self.game_instance.chosen_figure.color == Color.BLACK and "black_rook_1" not in chain(*self.game_instance.log) and "black_rook_1" in positions_dict:
-                        rook_position_num = figures_squares_now["black_rook_1"].numbered_equivalent
+                        rook_position_num = figures_squares_now["black_rook_1"].numerically
                     else:
                         return False
 
@@ -180,7 +182,7 @@ class King(Figure):
 
     @classmethod
     def in_safety(cls, chosen_figure, target_square):
-        """scans whether moving piece to target square threatens own king"""
+        """checks if piece move to target square threatens own king"""
         projected_positions = figures_squares_now.copy()
 
         attacked_piece = chosen_figure.detect_enemy(target_square)
@@ -205,7 +207,7 @@ class Rook(Figure):
 
     def validate_move(self, initial_square, target_square, positions_dict):
         """validates rook moves"""
-        initial, target = initial_square.numbered_equivalent, target_square.numbered_equivalent
+        initial, target = initial_square.numerically, target_square.numerically
         return (target.x - initial.x == 0 or target.y - initial.y == 0) and self.passage_free(initial, target, positions_dict)
 
 
@@ -215,7 +217,7 @@ class Bishop(Figure):
 
     def validate_move(self, initial_square, target_square, positions_dict):
         """validates bishop moves"""
-        initial, target = initial_square.numbered_equivalent, target_square.numbered_equivalent
+        initial, target = initial_square.numerically, target_square.numerically
         return abs(target.x - initial.x) == abs(target.y - initial.y) and self.passage_free(initial, target, positions_dict)
 
 
@@ -225,7 +227,7 @@ class Queen(Figure):
 
     def validate_move(self, initial_square, target_square, positions_dict):
         """validates queen moves"""
-        initial, target = initial_square.numbered_equivalent, target_square.numbered_equivalent
+        initial, target = initial_square.numerically, target_square.numerically
         return ((target.x - initial.x == 0 or target.y - initial.y == 0) and self.passage_free(initial, target, positions_dict)) \
             or (abs(target.x - initial.x) == abs(target.y - initial.y) and self.passage_free(initial, target, positions_dict))
 
@@ -236,16 +238,15 @@ class Knight(Figure):
 
     def validate_move(self, initial_square, target_square, positions_dict):
         """validates knight moves"""
-        initial, target = initial_square.numbered_equivalent, target_square.numbered_equivalent
+        initial, target = initial_square.numerically, target_square.numerically
         return abs(target.x - initial.x) in {1, 2} and abs(target.x - initial.x) + abs(target.y - initial.y) == 3
 
 
 # GENERATING OBJECTS FROM STRINGS INTO MULTIPLE REFERENCE DICTS; SAVING COPY FOR STARTING LAYOUT
 
 for square, coordinates in bp.squares_and_coordinates.items():
-    central_coordinates = bp.square_centres[square]
-    numbered_equivalent = NumberedEquivalent(bp.numeric_equivalent[square[0]], int(square[1]))
-    square_names_objs[square] = Square(square, coordinates, central_coordinates, numbered_equivalent)
+    central_coordinates = bp.square_centers[square]
+    square_names_objs[square] = Square(square, coordinates, central_coordinates)
 
 for piece in bp.pieces_and_positions:
     attributes = piece.split("_")
@@ -302,7 +303,7 @@ class Board:
         self.game_instance = None
 
     def click(self, event, game):
-        """establishes contact between types for mutual reference; dispatches GUI event to controller unit"""
+        """makes contact between types for mutual reference; sends GUI event to controller"""
         self.game_instance = game
         Figure.game_instance = game
         Square.game_instance = game
@@ -312,7 +313,7 @@ class Board:
         game.select_square(selected_coordinates)
 
     def show_possibilities(self):
-        """creates possibility-showing canvas objects and their ids"""
+        """makes possibility-showing canvas objects and their IDs"""
         game = self.game_instance
         possible_squares = []
         for square, obj in square_names_objs.items():
@@ -330,7 +331,7 @@ class Board:
             game.initial_square = None
 
     def hide_possibilities(self):
-        """deletes possibility-showing canvas objects by ids"""
+        """removes possibility-showing canvas objects by their IDs"""
         for square in square_names_objs:
             try:
                 self.canvas.delete(getattr(self, str(square)))
@@ -362,8 +363,9 @@ class Game:
             return Color.WHITE
 
     def choose_piece(self, selected_square):
-        """finds own piece to move, select attacked piece if own piece selected already"""
-        if (detected_figure := Figure.detect_figure(selected_square)):
+        """selects figure to move OR attack"""
+        detected_figure = Figure.detect_figure(selected_square)
+        if detected_figure:
             if self.detect_turn() is detected_figure.color:
                 self.initial_square = selected_square
                 self.chosen_figure = detected_figure
@@ -422,8 +424,8 @@ class Game:
         query_window.destroy()
 
     def make_move(self, target_square):
-        """removes captured figure from positions dict, makes move, dispatches to promotion, executes castling"""
-        initial, target = self.initial_square.numbered_equivalent, target_square.numbered_equivalent
+        """removes captured figure from positions db, makes move, sends to promotion, does castling"""
+        initial, target = self.initial_square.numerically, target_square.numerically
         x = (target.x - initial.x) * bp.TILE_LENGTH
         y = (initial.y - target.y) * bp.TILE_LENGTH
         if self.attacked_figure:
